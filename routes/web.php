92a85314a63b486c5387ad4bb1d6e1ac9c96b6b7
Rhayanntp <rhayanrhangel58@gmail.com>
2025-02-18 15:38:26 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Livewire\Tarefa\Create;
+use App\Livewire\Tarefa\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tarefa/create', Create::class);
+Route::get('/tarefa/create', Create::class);
+Route::get('/tarefa/index', Index::class);
